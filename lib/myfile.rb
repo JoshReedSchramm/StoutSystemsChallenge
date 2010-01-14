a50dd85df8bea12d1f32442834ded8e8c9ca2650
Josh Schramm <josh.schramm@gmail.com>
2010-01-14 17:34:22 -0500

    Added code we won with

---
+++ lib/myfile.rb
@@ -0,0 +1,42 @@
+output = ""
+output2 = []
+
+File.open("Input.txt") { |file| 
+ header = file.gets
+ 
+ output += header
+  
+ while(lines = file.gets)
+   fields = lines.split("\t")
+   
+   fields[1] = fields[1].gsub(/[^a-zA-Z]/, '').reverse
+   
+   if(!fields[1].empty?)
+    output2 << fields
+   end
+   
+ end
+  
+}
+
+
+#sort
+
+output2 = output2.sort { |x, y| 
+
+first = x[1] <=> y[1]
+
+if first == 0
+  first = x[5].to_f <=> y[5].to_f
+end
+
+ first
+  
+}
+
+
+output2.each { |line| 
+output +=   line.join("\t")
+  }
+
+File.open("output.txt", 'w') {|f| f.write(output) }
